ef6027476fb6ad8a33106aea707b09add7dd3e92
qwe5507 <apvmffkdls@gmail.com>
2022-08-15 11:34:05 +0900

    인터페이스 기반 프록시 - 적용

---
+++ src/main/java/hello/proxy/config/v1_config/InterfaceProxyConfig.java
@@ -0,0 +1,29 @@
+package hello.proxy.config.v1_config;
+
+import hello.proxy.app.v1.*;
+import hello.proxy.config.v1_config.interface_proxy.OrderControllerInterfaceProxy;
+import hello.proxy.config.v1_config.interface_proxy.OrderRepositoryInterfaceProxy;
+import hello.proxy.config.v1_config.interface_proxy.OrderServiceInterfaceProxy;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InterfaceProxyConfig {
+    @Bean
+    public OrderControllerV1 orderController(LogTrace logTrace) {
+        OrderControllerV1Impl controllerImpl = new OrderControllerV1Impl(orderService(logTrace));
+        return new OrderControllerInterfaceProxy(controllerImpl, logTrace);
+    }
+    @Bean
+    public OrderServiceV1 orderService(LogTrace logTrace) {
+        OrderServiceV1Impl serviceImpl = new OrderServiceV1Impl(orderRepository(logTrace));
+        return new OrderServiceInterfaceProxy(serviceImpl, logTrace);
+    }
+    @Bean
+    public OrderRepositoryV1 orderRepository(LogTrace logTrace) {
+        OrderRepositoryV1Impl repositoryImpl = new OrderRepositoryV1Impl();
+        return new OrderRepositoryInterfaceProxy(repositoryImpl, logTrace);
+    }
+
+}
